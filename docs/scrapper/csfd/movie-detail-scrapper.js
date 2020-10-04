1ff2b5e9dc01e47b96a36af830f25fa7acb86543
g43riko <gabrielcsollei@gmail.com>
2020-10-04 15:16:00 +0200

    add basic module

---
+++ docs/scrapper/csfd/movie-detail-scrapper.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var MovieDetailScrapper = /** @class */ (function () {
+    function MovieDetailScrapper() {
+        console.log("MovieDetailScrapper created");
+    }
+    return MovieDetailScrapper;
+}());
+exports.MovieDetailScrapper = MovieDetailScrapper;
---
+++ src/modules/scrapper/csfd/movie-detail-scrapper.ts
@@ -0,0 +1,5 @@
+export class MovieDetailScrapper {
+    public constructor() {
+    console.log("MovieDetailScrapper created");
+    }
+}
